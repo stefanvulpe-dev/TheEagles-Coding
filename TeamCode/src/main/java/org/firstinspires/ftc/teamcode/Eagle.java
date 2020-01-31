badc17893ddffb9ddbb8e1b092bb543816acedc7
Stefan <svulpe25@gmail.com>
2020-01-31 14:46:39 +0200

    Merge branch 'master' of C:\Users\Robert\Desktop\FTC_APP\SkyStone with conflicts.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Eagle.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Eagle.java
@@ -454,12 +454,8 @@ public class Eagle {
 
     }
 
-    private void takeSkyStone() {
+    public void takeSkyStone() {
         servoBlue.setPosition(0.48);
-    }
-
-    private void leaveSkyStone() {
-        servoBlue.setPosition(0);
     }
 
     public void makeSampleBlue() throws InterruptedException {
@@ -526,44 +522,20 @@ public class Eagle {
         switch (position) {
             case "left" :
                 strafeBackward(35);
-                navigateRight(160);
-                leaveSkyStone();
-                sleep(250);
+                navigateRight(180);
                 //Ma intorc dupa celalalt
-                navigateLeft(235);
-                strafeForward(35);
-                takeSkyStone();
-                sleep(100);
-                strafeBackward(35);
-                navigateRight(210);
             break;
 
             case "right" :
                 strafeBackward(35);
-                navigateRight(130);
-                leaveSkyStone();
+                navigateRight(80);
                 //Ma intorc dupa celalalt
-                sleep(250);
-                navigateLeft(195);
-                strafeForward(35);
-                takeSkyStone();
-                sleep(100);
-                strafeBackward(35);
-                navigateRight(190);
             break;
 
             case "center" :
                 strafeBackward(35);
                 navigateRight(140);
-                leaveSkyStone();
-                sleep(250);
                 //Ma intorc dupa celalalt
-                navigateLeft(210);
-                strafeForward(35);
-                takeSkyStone();
-                sleep(100);
-                strafeBackward(35);
-                navigateRight(230);
             break;
         }
 
